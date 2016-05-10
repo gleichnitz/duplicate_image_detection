f7aee1275aa324a72d78352b9e8c9a269d25c329
James Evans <jaevans@princeton.edu>
2016-05-10 18:22:12 -0400

    add prediction stuff

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from dup_eval import predict
 
-from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def predict_class(raw_img):
+	# img = request.POST["img"]
+	pred = predict(raw_img)
+	pred_delim = pred.split(',')[1:]
+	probs = []
+	i = 0
+	for p in pred_delim:
+		probs.append((p, i))
+		i += 1
 
-def db(request):
+	probs = sorted(probs, reverse=True)
+	toRet = {'results':probs}
+	return JsonResponse(toRet)
 
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+# def db(request):
+#     greeting = Greeting()
+#     greeting.save()
+#     greetings = Greeting.objects.all()
+#     return render(request, 'db.html', {'greetings': greetings})
 
